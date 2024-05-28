83ecee43d20f71b660541e79470551291b721e2a
DESKTOP-LIRSJS6\Klova <nikolayklova404@gmail.com>
2024-05-28 19:32:25 +0300

    fix rate creating page

--- src/pages/reservations/CreateReservation.vue
+++ src/pages/reservations/CreateReservation.vue
@@ -9,9 +9,15 @@ import reservationService from '@/js/services/reservationService'
 import isVarEmpty from '@/js/helper'
 import { operationTypes } from '@/js/consts/operationTypesConsts'
 import { reservationStatuses } from '@/js/consts/reservationStatusesConsts'
+import { validationRules } from '@/js/validationRules.js'
 
 const router = useRouter()
 const snackbar = useSnackbar()
+
+const isLoading = ref(false)
+
+const isFormValid = ref(false)
+const form = ref(null)
 
 const contacts = ref([])
 const currencies = ref([])
@@ -35,6 +41,10 @@ const rateTextValue = computed(() => {
 })
 
 const isAmountDisabled = computed(() => {
+  if (step.value > 1) {
+    return true
+  }
+
   return currency.value === null || operationType.value === null
 })
 
@@ -46,6 +56,10 @@ const nextOrSubmit = async () => {
   if (isLastItem.value) {
     return await createReservation()
   }
+  if (step.value === 1 && !isFormValid.value) {
+    form.value.validate()
+    return
+  }
   step.value++
 }
 
@@ -53,6 +67,8 @@ onMounted(async () => {
   await setContacts()
   await setCurrencies()
 })
+
+//region watchers
 
 watch(amount, (newValue) => {
   returnedAmount.value = Math.round((newValue * rateTextValue.value) * 100) / 100
@@ -65,6 +81,10 @@ watch(currency, async (newValue) => {
   rate.value = null
   rateTextValue.value = null
 })
+
+//endregion
+
+//region value setters
 
 async function setContacts() {
   await contactService.getContacts()
@@ -87,6 +107,69 @@ async function getRate(currencyId) {
     })
 }
 
+//endregion
+
+const isFieldDisabled = computed(() => {
+  if (step.value > 1) {
+    return true
+  }
+  return false
+})
+
+//region validation rules
+
+const contactValidationRules = computed(() => {
+  let model = contact.value
+  let rules = []
+
+  rules.push(validationRules.isFieldRequired(model))
+  return rules
+})
+
+const currencyValidationRules = computed(() => {
+  let model = currency.value
+  let rules = []
+
+  rules.push(validationRules.isFieldRequired(model))
+  return rules
+})
+
+const reservationStatusValidationRules = computed(() => {
+  let model = reservationStatus.value
+  let rules = []
+
+  rules.push(validationRules.isFieldRequired(model))
+  return rules
+})
+
+const operationTypeValidationRules = computed(() => {
+  let model = operationType.value
+  let rules = []
+
+  rules.push(validationRules.isFieldRequired(model))
+  return rules
+})
+
+const amountValidationRules = computed(() => {
+  let model = amount.value
+  let rules = []
+
+  if(!isAmountDisabled.value) {
+    rules.push(validationRules.isFieldRequired(model))
+
+    let isPositiveAmount = typeof parseFloat(model) === 'number' && !isNaN(parseFloat(model)) && parseFloat(model) > 0
+    rules.push((isPositiveAmount) || 'Amount must be a positive number')
+  }
+
+  return rules
+})
+
+//endregion
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function createReservation() {
   let data = {
     contactId: contact.value.id,
@@ -95,14 +178,27 @@ async function createReservation() {
     statusId: reservationStatus.value.id,
     amount: amount.value,
   }
-
-  await reservationService.createReservation(data)
-    .then(response => {
-      console.log(response)
+  isLoading.value = true
+  let reservationCreatedSuccessfully = false
+  try {
+    await reservationService.createReservation(data)
+    snackbar.add({
+      type: 'success',
+      text: `Reservation created successfully!`,
     })
-    .catch(error => {
-      console.log(error)
+    reservationCreatedSuccessfully = true
+  } catch (error) {
+    snackbar.add({
+      type: 'error',
+      text: error.message,
     })
+  } finally {
+    isLoading.value = false
+    if(reservationCreatedSuccessfully) {
+      await delay(1000)
+      await router.push('/transactions/get')
+    }
+  }
 }
 </script>
 
@@ -111,168 +207,92 @@ async function createReservation() {
     v-model="step"
     :items="items"
   >
-    <VStepperWindow v-if="step === 1">
-      <VStepperHeader class="text-h3">
-        Reservation Information
-      </VStepperHeader>
-
-      <VStepperWindow>
-        <VRow>
-          <VCol class="pa-5">
-            <VAutocomplete
-              v-model="contact"
-              label="Contact"
-              :items="contacts"
-              item-title="fullName"
-              clearable
-              return-object
-            />
-          </VCol>
-        </VRow>
-        <VRow>
-          <VCol class="pa-5">
-            <VAutocomplete
-              v-model="currency"
-              label="Currency"
-              :items="currencies"
-              item-title="code"
-              return-object
-              clearable
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VSelect
-              v-model="operationType"
-              label="Operation Type"
-              :items="operationTypes"
-              item-title="name"
-              return-object
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VSelect
-              v-model="reservationStatus"
-              label="Reservation status"
-              :items="reservationStatuses"
-              item-title="name"
-              return-object
-            />
-          </VCol>
-        </VRow>
-        <VRow>
-          <VCol>
-            <VTextField
-              v-model="amount"
-              hide-details="auto"
-              type="number"
-              label="Amount"
-              :disabled="isAmountDisabled"
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VTextField
-              v-model="rateTextValue"
-              hide-details="auto"
-              type="number"
-              label="Rate"
-              disabled
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VTextField
-              v-model="returnedAmount"
-              hide-details="auto"
-              type="number"
-              label="Returned amount"
-              disabled
-            />
-          </VCol>
-        </VRow>
-      </VStepperWindow>
-    </VStepperWindow>
-
-    <VStepperWindow v-if="step === 2">
-      <VStepperHeader class="text-h3">
-        Confirmation
-      </VStepperHeader>
-
-      <VStepperWindow>
-        <VRow>
-          <VCol class="pa-5">
-            <VAutocomplete
-              v-model="contact"
-              label="Contact"
-              :items="contacts"
-              item-title="fullName"
-              clearable
-              return-object
-              disabled
-            />
-          </VCol>
-        </VRow>
-        <VRow>
-          <VCol class="pa-5">
-            <VAutocomplete
-              v-model="currency"
-              label="Currency"
-              :items="currencies"
-              item-title="code"
-              return-object
-              clearable
-              disabled
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VSelect
-              v-model="operationType"
-              label="Operation Type"
-              :items="operationTypes"
-              item-title="name"
-              return-object
-              disabled
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VSelect
-              v-model="reservationStatus"
-              label="Reservation status"
-              :items="reservationStatuses"
-              item-title="name"
-              return-object
-              disabled
-            />
-          </VCol>
-        </VRow>
-        <VRow>
-          <VCol>
-            <VTextField
-              v-model="amount"
-              hide-details="auto"
-              type="number"
-              label="Amount"
-              disabled
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VTextField
-              v-model="rateTextValue"
-              hide-details="auto"
-              type="number"
-              label="Rate"
-              disabled
-            />
-          </VCol>
-          <VCol class="pa-5">
-            <VTextField
-              v-model="returnedAmount"
-              hide-details="auto"
-              type="number"
-              label="Returned amount"
-              disabled
-            />
-          </VCol>
-        </VRow>
-      </VStepperWindow>
+    <VStepperWindow>
+        <VForm
+          v-model="isFormValid"
+          ref="form"
+        >
+          <VRow>
+            <VCol class="pa-5">
+              <VAutocomplete
+                v-model="contact"
+                label="Contact"
+                :items="contacts"
+                item-title="fullName"
+                clearable
+                :rules="contactValidationRules"
+                return-object
+                :disabled="isFieldDisabled"
+              />
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol class="pa-5">
+              <VAutocomplete
+                v-model="currency"
+                label="Currency"
+                :items="currencies"
+                item-title="code"
+                return-object
+                :rules="currencyValidationRules"
+                clearable
+                :disabled="isFieldDisabled"
+              />
+            </VCol>
+            <VCol class="pa-5">
+              <VSelect
+                v-model="operationType"
+                label="Operation Type"
+                :items="operationTypes"
+                item-title="name"
+                :rules="operationTypeValidationRules"
+                return-object
+                :disabled="isFieldDisabled"
+              />
+            </VCol>
+            <VCol class="pa-5">
+              <VSelect
+                v-model="reservationStatus"
+                label="Reservation status"
+                :items="reservationStatuses"
+                item-title="name"
+                :rules="reservationStatusValidationRules"
+                return-object
+                :disabled="isFieldDisabled"
+              />
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol>
+              <VTextField
+                v-model="amount"
+                hide-details="auto"
+                type="number"
+                label="Amount"
+                :rules="amountValidationRules"
+                :disabled="isAmountDisabled"
+              />
+            </VCol>
+            <VCol class="pa-5">
+              <VTextField
+                v-model="rateTextValue"
+                hide-details="auto"
+                type="number"
+                label="Rate"
+                disabled
+              />
+            </VCol>
+            <VCol class="pa-5">
+              <VTextField
+                v-model="returnedAmount"
+                hide-details="auto"
+                type="number"
+                label="Returned amount"
+                disabled
+              />
+            </VCol>
+          </VRow>
+        </VForm>
     </VStepperWindow>
 
     <template #next>
@@ -286,9 +306,23 @@ async function createReservation() {
     </template>
   </VStepper>
 
+  <VDialog
+    v-model="isLoading"
+    :persistent="isLoading"
+  >
+    <VProgressCircular
+      v-if="isLoading"
+      color="primary"
+      :size="100"
+      :width="10"
+      indeterminate
+      class="ma-auto"
+    />
+  </VDialog>
+
   <Vue3Snackbar
     bottom
     right
-    :duration="500"
+    :duration="2000"
   />
 </template>
